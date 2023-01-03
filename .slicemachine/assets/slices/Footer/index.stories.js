6ea733fb11f50ffd56f690104ed35c1065405a75
Niraj Niroula <nirajniroula@nn-mbp.local>
2023-01-03 11:00:43 +0545

    init

---
+++ .slicemachine/assets/slices/Footer/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Footer';
+
+export default {
+  title: 'slices/Footer'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"footer","items":[],"primary":{"title":[{"type":"heading1","text":"Deliver scalable systems","spans":[]}],"description":[{"type":"paragraph","text":"Amet sit duis irure culpa nisi ad laborum ea excepteur. Eu nostrud consectetur non minim reprehenderit laborum nisi fugiat minim voluptate ut elit pariatur. Occaecat pariatur culpa mollit in laboris cillum.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ .slicemachine/assets/slices/TestSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TestSlice';
+
+export default {
+  title: 'slices/TestSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"test_slice","items":[],"primary":{"username":"whiteboard dot-com convergence","password":"engineer compelling experiences"},"id":"_Default"}} />
+_Default.storyName = 'Default'
