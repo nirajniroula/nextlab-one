76a20dac3473d123749f9bfa57e59de012351a24
Niraj Niroula <niniroula@gmail.com>
2023-05-31 14:21:07 +0545

    trigger deploy by changed document's tag

--- pages/api/prismic-webhook.js
+++ pages/api/prismic-webhook.js
@@ -4,35 +4,11 @@ import bodyParser from "body-parser";
 // Configure bodyParser to parse webhook payload
 const jsonParser = bodyParser.json();
 
-// Define your desired tag name
-const desiredBuild = "not-x";
-
-// // Define your build logic here
-// const triggerBuild = () => {
-//   // Replace the build command with your specific build command
-//   const buildCommand = "npm run build";
-
-//   // Run the build command
-//   exec(buildCommand, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Build command failed: ${error}`);
-//       return;
-//     }
-//     console.log(`Build output: ${stdout}`);
-//   });
-// };
-
-// Define your build logic here
-const triggerBuild = async () => {
-  // Replace the deploy hook URL with your actual deploy hook URL
-  const deployHookUrl =
-    "https://api.vercel.com/v1/integrations/deploy/prj_qdZTrbbo3MIhhxbyTIdcDXITsFKR/5NZNj5uLUM?buildCache=false";
-
-  // Make an HTTP POST request to the deploy hook URL
-  console.log(">>>>axios call...");
+const callDeployHookUrl = async (url) => {
+  console.log(">>>>Deploy starting...");
 
   await axios
-    .post(deployHookUrl)
+    .post(url)
     .then((response) => {
       console.log("Deploy hook called successfully");
       console.log("Response:", response.data);
@@ -40,6 +16,51 @@ const triggerBuild = async () => {
     .catch((error) => {
       console.error("Failed to call deploy hook:", error);
     });
+};
+// Define your build logic here
+const triggerBuildSiteOne = async () => {
+  // Replace the deploy hook URL with your actual deploy hook URL
+  const deployHookUrl =
+    "https://api.vercel.com/v1/integrations/deploy/prj_qdZTrbbo3MIhhxbyTIdcDXITsFKR/5NZNj5uLUM?buildCache=false";
+  console.log(">>>>Deploy site 1...");
+
+  callDeployHookUrl(deployHookUrl);
+};
+
+// Define your build logic here
+const triggerBuildSiteTwo = async () => {
+  // Replace the deploy hook URL with your actual deploy hook URL
+  const deployHookUrl =
+    "https://api.vercel.com/v1/integrations/deploy/prj_LVh6gyfTqVH51mMu5q4HQ1Yj9rSA/fTKzCNlDlw";
+
+  // Make an HTTP POST request to the deploy hook URL
+  console.log(">>>>Deploy site 2...");
+  callDeployHookUrl(deployHookUrl);
+};
+
+// Prismic API configuration
+const prismicApiEndpoint = process.env.PRISMIC_API;
+const prismicAccessToken = process.env.PRISMIC_ACCESS_TOKEN;
+
+// Function to fetch document IDs based on tags
+const fetchDocumentIdsByTags = async (tags) => {
+  try {
+    const api = await Prismic.getApi(prismicApiEndpoint, {
+      accessToken: prismicAccessToken,
+    });
+
+    const response = await api.query(
+      Prismic.Predicates.at("document.tags", tags),
+      { pageSize: 100, fetch: [] }
+    );
+
+    const documentIds = response.results.map((document) => document.id);
+
+    return documentIds;
+  } catch (error) {
+    console.error("Failed to fetch document IDs:", error);
+    return [];
+  }
 };
 
 export default function handler(req, res) {
@@ -50,19 +71,34 @@ export default function handler(req, res) {
 
     jsonParser(req, res, async () => {
       //   var resObj = eval("(" + req.body + ")");
-      console.log(">>>>secret", req.body.secret);
 
-      const { secret } = req.body;
+      const { documents, secret } = req.body;
 
-      // Check if the desired tag name is present in the webhook payload
-      if (secret === desiredBuild) {
+      // Check if the secret is present in the webhook payload
+      if (secret === "secret123") {
+        // Fetch the document IDs based on tags
+        const desiredDocumentsIds = await fetchDocumentIdsByTags(tags).catch(
+          (error) => {
+            console.error("Failed to fetch document IDs:", error);
+          }
+        );
+
         // Trigger the build process
+
         console.log(">>>>triggering...");
-
-        await triggerBuild();
+        if (Array.isArray(desiredDocumentsIds) && Array.isArray(documents)) {
+          for (const documentId of desiredDocumentIds) {
+            if (documents.includes(documentId)) {
+              // Trigger the build process
+              await triggerBuildSiteOne();
+              break; // If one desired document is found, no need to continue checking the rest
+            }
+          }
+        }
+        res.status(200).end();
+      } else {
+        res.status(401).end();
       }
-
-      res.status(200).end();
     });
   } else {
     res.status(405).end();
